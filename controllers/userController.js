02eac0beac03384da2f5ddcb26d863e61fea58b1
Manya Awasthi <manya.awasthi.24cse@bmu.edu.in>
2025-11-18 19:13:29 +0530

    signin signup page functional now

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,6 +85,12 @@ exports.login = async (req, res) => {
 
         const token = generateToken(user._id);
 
+        // Set token as cookie for server-side auth
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
         res.status(200).json({
             success: true,
             message: 'Login successful',
